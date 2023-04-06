825aeb8cca8b72ab003d23807991d9e14bc974b2
allanmg <flaafmg@gmail.com>
2023-04-06 17:46:43 -0300

    started cartrige code

--- NES/Cartrige.cs
+++ NES/Cartrige.cs
@@ -8,7 +8,30 @@ namespace SharpNES.NES
 {
     public class Cartrige
     {
+        byte mapperID = 0;
+        byte PRGBanks = 0;
+        byte CHRBanks = 0;
+        public Cartrige(String path)
+        {
 
+        }
+
+
+        struct header
+        {
+            char[] name = new char[4];
+            byte prgChunks;
+            byte chrChunks;
+            byte mapper1;
+            byte mapper2;
+            byte prgRamSize;
+            byte tvSys1;
+            byte tvSys2;
+
+            public header()
+            {
+            }
+        }
 
     }
 }
